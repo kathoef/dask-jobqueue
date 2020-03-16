ff6a40c4c4c03de25ca0763ce3c55d68db9c103c
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2020-03-16 17:23:04 +0100

    Merge both tests and add config based test

--- dask_jobqueue/tests/test_network_scopes.py
+++ dask_jobqueue/tests/test_network_scopes.py
@@ -1,18 +1,23 @@
-import dask_jobqueue, pytest
+import dask_jobqueue, pytest, dask
 
 def test_allow_locally_non_existent_worker_interface():
-    """This ensures that setting an interface that is only available on compute nodes is possible. This interface should never be evaluated elsewhere."""
+    """Ensure setting interface only available on a compute node is possible.
+    This interface should never be evaluated in any other than the Dask worker
+    network scope, i.e. the scheduler network location should be set independently.
+    """
     with dask_jobqueue.SLURMCluster(
         cores=8, memory='24GB',
         interface='locally-non-existent-interface',
     ) as cluster:
-        pass
+        job_script = cluster.job_script()
+        assert "locally-non-existent-interface" in job_script
 
-def test_if_non_existent_interface_is_correctly_passed_to_job_script():
-    """This ensures that the locally non-existent interface is correctly passed to the job script. IP address retrieval for the interface should happen with the Dask worker call."""
+
+def test_allow_locally_non_existent_worker_interface_via_config_file():
+    """This ensures the above behaviour for passing the option via a jobqueue configuration file."""
+    dask.config.set({'jobqueue.slurm.interface':'locally-non-existent-interface'})
     with dask_jobqueue.SLURMCluster(
         cores=8, memory='24GB',
-        interface='locally-non-existent-interface',
     ) as cluster:
         job_script = cluster.job_script()
         assert "locally-non-existent-interface" in job_script
